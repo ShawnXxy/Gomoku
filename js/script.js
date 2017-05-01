c8e2c2afcde6c4267c76914cf39180622b3f9938
ShawnXxy <shawnxxy@hotmail.com>
2017-05-01 11:32:19 -0500

    AI updated

--- js/script.js
+++ js/script.js
@@ -3,20 +3,15 @@
  */
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
-var gameover = false;
-// // draw diagon
-// context.moveTo(0, 0); // start from left top
-// context.lineTo(450, 450); // end at right bottom
-// context.stroke();
 
-// Draw board : 24 cells in a row with 30px per cell and 15px margin
+// Draw board : 24 cells in a row with 30px per cell and 15px margin on left/right side
 var drawChessBoard = function() {
     for (var i = 0; i < 25; i++) {
-        // row
+        // columns
         context.moveTo(15 + i * 30, 15);
         context.lineTo(15 + i * 30, 735);
         context.stroke();
-        // column
+        // rows
         context.moveTo(15, 15 + i * 30);
         context.lineTo(735, 15 + i * 30);
         context.stroke();
@@ -26,50 +21,14 @@ var drawChessBoard = function() {
 };
 drawChessBoard();
 
-// Draw peices
-// var drawPeices = function() {
-//     context.beginPath();
-//     context.arc(200, 200, 100, 0, 2 * Math.PI); // lat, lon, radius, start angle, end angle
-//     context.closePath();
-//     // add pieces visual effect to make them look real
-//     var gradient = context.createRadialGradient(200 + 2, 200 - 2, 50, 200, 200, 20);
-//     gradient.addColorStop(0, "#0A0A0A");
-//     gradient.addColorStop(1, "#636766");
-//     context.fillStyle = gradient;
-//     context.fill();
-// };
-// drawPeices();
-
 /**
- * DRAW PIECES
- * @param {*} i = lat
- * @param {*} j = lon
- * @param {*} me = which side (black or white)
+ * INITIALIZING
  */
-var oneStep = function(i, j, me) { 
-    //// Draw peices
-    context.beginPath();
-    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI); // lat, lon, radius, start angle, end angle
-    context.closePath();
-    // add pieces visual effect to make them look real
-    var gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0);
-    if (me) {
-        gradient.addColorStop(0, "#0A0A0A");
-        gradient.addColorStop(1, "#636766");
-    } else {
-        gradient.addColorStop(0, "#D1D1D1");
-        gradient.addColorStop(1, "#F9F9F9");
-    }
-    
-    context.fillStyle = gradient;
-    context.fill();
-};
-// TEST:
-// oneStep(0, 0, true);
-// oneStep(1, 1, false);
+var me = true;
+var gameover = false;
 
 /**
- * EVENT : PLACE A PIECE WHEN CLICK ON BOARD
+ * WINNING RULES
  */
 // store all possible positions on board into array
 var position = [];
@@ -79,45 +38,8 @@ for (var i = 0; i < 25; i++) {
         position[i][j] = 0; // no piece placed on current position
     }
 }
-// initialize black starts first
-var me = true; 
-chess.onclick = function(event) {
-    if (gameover) {
-        return;
-    }
-    if (!me) {
-        return;
-    }
-    var x = event.offsetX;
-    var y = event.offsetY;
-    var i = Math.floor(x / 30);
-    var j = Math.floor(y / 30);
-    // check if current position is available
-    if (position[i][j] == 0) {
-        oneStep(i, j, me);
-        position[i][j] = 1;
 
-        // AI check
-        for (var k = 0; k < count; k++) {
-            if (wins[i][j][k]) {
-                myWin[k]++;
-                computerWin[k] = 6; //exception
-                if (myWin[k] == 5) {
-                    window.alert("YOU WIN!");
-                    gameover = true;
-                }
-            }
-        } // end of AI
-        if (!gameover) {
-            me = !me;
-            computerAI();
-        }
-    }   
-} // end onclick
-
-/**
- * AI implementation
- */
+// winning algorithm
 var wins = [];
 for (var i = 0; i < 25; i++) {
     wins[i] = [];
@@ -162,8 +84,6 @@ for (var i = 0; i < 21; i++) {
         count++;
     }
 };
-// test
-// console.log(count);
 
 var myWin = [];
 var computerWin = [];
@@ -172,6 +92,71 @@ for (var i = 0; i < count; i++) {
     computerWin[i] = 0;
 }
 
+
+/**
+ * EVENT : PLACE A PIECE WHEN CLICK ON BOARD
+ * 
+ * @param {*} i = lat
+ * @param {*} j = lon
+ * @param {*} me = which side (black or white)
+ * 
+ */
+var oneStep = function(i, j, me) { 
+    //// Draw peices
+    context.beginPath();
+    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI); // lat, lon, radius, start angle, end angle
+    context.closePath();
+    // add pieces visual effect to make them look real
+    var gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 - 2, 13, 15 + i * 30 + 2, 15 + j * 30 - 2, 0);
+    if (me) {
+        gradient.addColorStop(0, "#0A0A0A");
+        gradient.addColorStop(1, "#636766");
+    } else {
+        gradient.addColorStop(0, "#D1D1D1");
+        gradient.addColorStop(1, "#F9F9F9");
+    }
+    context.fillStyle = gradient;
+    context.fill();
+};
+
+chess.onclick = function(event) {
+    if (gameover) {
+        return;
+    } 
+    if (!me) {
+        return;
+    }
+
+    var x = event.offsetX;
+    var y = event.offsetY;
+    var i = Math.floor(x / 30);
+    var j = Math.floor(y / 30);
+    // check if current position is available
+    if (position[i][j] == 0) {
+        oneStep(i, j, me);
+        position[i][j] = 1;
+
+        // AI check
+        for (var k = 0; k < count; k++) {
+            if (wins[i][j][k]) {
+                myWin[k]++;
+                computerWin[k] = 6; //exception
+                if (myWin[k] == 5) {
+                    window.alert("YOU WIN!");
+                    gameover = true;
+                }
+            }
+        } // end of AI
+        if (!gameover) {
+            me = !me;
+            computerAI();
+        }
+    } // end of position check
+} // end onclick
+
+/**
+ * AI implementation
+ */
 var computerAI = function() {
     var myScore = [];
     var computerScore = [];
@@ -179,7 +164,7 @@ var computerAI = function() {
     var u = 0, v = 0; //maxScore[u][v]
     for (var i = 0; i < 25; i++) {
         myScore[i] = [];
-        computerAI[i] = [];
+        computerScore[i] = [];
         for (var j = 0; j < 25; j++) {
             myScore[i][j] = 0;
             computerScore[i][j] = 0;
@@ -187,29 +172,32 @@ var computerAI = function() {
     }
     for (var i = 0; i < 25; i++) {
         for (var j = 0; j < 25; j++) {
-            if (position[i][j] == 0) {
+            if (position[i][j] == 0) { // if current position is playable
                 for (var k = 0; k < count; k++) {
                     if (wins[i][j][k]) {
+                        //player
                         if (myWin[k] == 1) {
                             myScore[i][j] += 200;
-                        } else if (myWin[K] == 2) {
+                        } else if (myWin[k] == 2) {
                             myScore[i][j] += 400;
                         } else if (myWin[k] == 3) {
                             myScore[i][j] += 2000;
-                        } else if (myWin[i][j] == 4) {
+                        } else if (myWin[k] == 4) {
                             myScore[i][j] += 10000;
                         }
+                        // AI
                         if (computerWin[k] == 1) {
                             computerScore[i][j] += 220;
-                        } else if (computerWin[K] == 2) {
+                        } else if (computerWin[k] == 2) {
                             computerScore[i][j] += 420;
                         } else if (computerWin[k] == 3) {
                             computerScore[i][j] += 2100;
-                        } else if (computerWin[i][j] == 4) {
+                        } else if (computerWin[k] == 4) {
                             computerScore[i][j] += 20000;
                         }
                     }
                 }
+
                 // player
                 if (myScore[i][j] > maxScore) {
                     maxScore = myScore[i][j];
@@ -220,10 +208,11 @@ var computerAI = function() {
                         v = i;
                         u = j;
                     }
-                }
+                } // end of player score
+
                 // AI
                 if (computerScore[i][j] > maxScore) {
-                    computerScore = myScore[i][j];
+                    maxScore = computerScore[i][j];
                     v = i;
                     u = j;
                 } else if (computerScore[i][j] == maxScore) {
@@ -231,10 +220,11 @@ var computerAI = function() {
                         v = i;
                         u = j;
                     }
-                }
+                } // end of AI score
             }
         }
-    }
+    } // end of computerAI()
+
     // where AI placed pieces
     oneStep(u, v, false);
     position[u][v] = 2;
